cd898d827cb22d06f17ba5747c52e2e0014aa476
amueller8 <68559641+amueller8@users.noreply.github.com>
2021-06-21 20:48:38 -0400

    adding color palettes as option

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 import {ColorCard} from './components/colorCard.js';
 
-function createCards(){
-  document.getElementById('root').append(<ColorCard onclick={this.generateColor}/>);
-  document.getElementById('color-zone').append(<h1>hi</h1>);
-}
+
+
 function App() {
+  const [paletteMode, togglePaletteMode] = useState(false);
   return (
     <div className="App" id = "color-zone">
       <h1>Generate a random color</h1>
-      <ColorCard />
-      <button onclick={createCards}>Go</button>
+      <ColorCard id = "hey"/>
+      
+      <button onClick = {() => togglePaletteMode}>
+        Enter random palette mode
+      </button>
+
     </div>
   );
 }
--- colors-app/src/components/colorCard.js
+++ colors-app/src/components/colorCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+
+
 export class ColorCard extends React.Component{
-    constructor(props){
+    
+
+    constructor (props) {
         super(props);
         this.state = {color: "#" + Math.floor(Math.random()*16777215).toString(16)};
         this.generateColor = this.generateColor.bind(this);
@@ -11,29 +15,70 @@ export class ColorCard extends React.Component{
     generateColor(){
         // borrowed color generator from https://css-tricks.com/snippets/javascript/random-hex-color/
         const newColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-        this.setState({color: newColor});
+        this.setState((state) => {
+            return {color: newColor}
+            
+        });
 
     }
 
     render(){
-        const styles = {
+        const colorStyles = {
             width: "50%",
             margin: "auto",
             height: "50vh",
             fontFamily: "Helvetica", //if I was being fancy i'd get helvetica nueue
             border: "1px solid black"
-
+        
         };
-        return(
-        <div onclick = {this.generateColor} style = {styles}>
-            <div style={{backgroundColor: this.state.color, height: "80%"}}>
-            </div>
-            <div style= {{backgroundColor: 'white'}}>
-                <h2>{this.state.color}</h2>
-            </div>
-        </div>
-        );
+
+        const paletteStyle = {
+            width: "25%",
+            margin: "auto",
+            height: "50vh",
+            fontFamily: "Helvetica", //if I was being fancy i'd get helvetica nueue
+            border: "1px solid black"
+
+        }
+
+    
+
+        if(!this.props.paletteMode){
+            return(
+                <div > 
+                <div style = {colorStyles}>
+                    <div style={{backgroundColor: this.state.color, height: "80%"}}>
+                    </div>
+                    <div style= {{backgroundColor: 'white'}}>
+                        <h2>{this.state.color}</h2>
+                    </div>
+                </div>
+                <button onClick = {this.generateColor} style = {{margin: "2em"}}>New Color </button>
+                </div>
+                
+                );
+
+        } else {
+            //palette version
+            return(
+                <div > 
+                <div style = {paletteStyle}>
+                    <div style={{backgroundColor: this.state.color, height: "80%"}}>
+                    </div>
+                    <div style= {{backgroundColor: 'white'}}>
+                        <h2>{this.state.color}</h2>
+                    </div>
+                </div>
+                
+                </div>
+                
+            );
+        }
+        
 
     }
 
 }
+
+
+
